06448b5d1f39977be6467e17d50028a51d426866
juugeebee <july2023@yahoo.fr>
2021-02-09 16:15:28 +0100

    tabulations ok!

--- targets_preparation.sh
+++ targets_preparation.sh
@@ -9,17 +9,17 @@ echo ""
 
 #### Préparation fichiers bed
 
-DIC="/media/Data1/jbogoin/ref/hg38_Mlast/hg38_GenDev.dict"
+DIC="/media/Data1/jbogoin/ref/fa_hg38/hg38_GenDev/hg38_GenDev.dict"
 
 # Télécharger le fichier gencode.v34.basic.annotation.gff3 sur le site de GENECODE
 #wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/gencode.v34.basic.annotation.gff3.gz
 
 # Recupérer les CDS
-zgrep " CDS " gencode.v34.basic.annotation.gff3.gz > gencode.v34.basic.annotation.CDS.gff3
-
 # Ajouter une 4ème colonne et trier
-awk -F "\t" '{print $1"\t"$4-1"\t"$5}' gencode.v34.basic.annotation.CDS.gff3 \
-    | sort -Vu > gencode.v34.basic.annotation.CDS.bed
+
+zcat gencode.v34.basic.annotation.gff3.gz | \
+    awk -F "\t" '{if ($3=="CDS"){print $1"\t"$4-1"\t"$5}}' |\
+    sort -Vu > gencode.v34.basic.annotation.CDS.bed
 
 # Supprimer les intervalles chevauchants
 bedtools merge -i gencode.v34.basic.annotation.CDS.bed > gencode.v34.basic.annotation.CDS.merged.bed 
