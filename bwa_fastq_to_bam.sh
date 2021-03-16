5f99171d45c5f13533f91304375b58aaf678e45a
juugeebee <july2023@yahoo.fr>
2021-03-16 10:23:12 +0100

    hg38

--- bwa_fastq_to_bam.sh
+++ bwa_fastq_to_bam.sh
@@ -4,10 +4,10 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate fastq_bam_env
 
 # hg38
-#REF="/media/Data1/jbogoin/ref/fa_hg38/hg38_GenDev/hg38_GenDev.fa"
+REF="/media/Data1/jbogoin/ref/fa_hg38/hg38_GenDev/hg38_GenDev.fa"
 
 # hg19
-REF="/media/Data1/jbogoin/ref/hg19_ref/hg19_std.fa"
+#REF="/media/Data1/jbogoin/ref/hg19_ref/hg19_std.fa"
 
 echo ""
 echo "bwa_fastq_to_bam.sh start"
@@ -16,11 +16,11 @@ echo ""
 #rm *.bam
 
 ## MAPPING BWA SEQUENTIAL ###
-for R1 in *_R1_001.fastq.gz; 
+for R1 in *.R1.fastq.gz; 
 
-    do R2=${R1/_R1/_R2}; 
+    do R2=${R1/.R1/.R2}; 
 
-    SAMPLE=${R1%%_*}; 
+    SAMPLE=${R1%%.*}; 
 
     FLOWCELL="$(zcat $R1 | head -1 | awk '{print $1}' | cut -d ":" -f 3)"; 
 
@@ -28,14 +28,14 @@ for R1 in *_R1_001.fastq.gz;
 
     BARCODE="$(zcat $R1 | head -1 | awk '{print $2}' | cut -d ":" -f 4)"; 
 
-    RG=$(echo "\"@RG\tID:${DEVICE}.${FLOWCELL}.${SAMPLE}\tPU:${FLOWCELL}\tSM:${SAMPLE}\tPL:ILLUMINA\tLB:${SAMPLE}-${BARCODE}\"")
+    RG=$(echo "\"@RG\tID:${DEVICE}.${FLOWCELL}.${SAMPLE}\tPU:${FLOWCELL}\tSM:${SAMPLE}\tPL:ILLUMINA\tLB:${SAMPLE}-${BARCODE}\"");
     
     MAPPING_CMD=$(echo "bwa mem -M -t 24 -R $RG $REF $R1 $R2 \
     | samtools sort -@ 6 -o ${SAMPLE}.bam -");
 
     eval $MAPPING_CMD;
 
- done;
+done;
 
 ## MARK DUPLICATES
 for i in *.bam; 
