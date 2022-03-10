462bed0bc7430bd547d05938eef559828335d047
juugeebee <july2023@yahoo.fr>
2022-03-10 10:01:53 +0100

    working well

--- bcl2fastq.sh
+++ bcl2fastq.sh
@@ -10,12 +10,12 @@ echo ""
 bcl2fastq --barcode-mismatches 0 --minimum-trimmed-read-length 35 --no-lane-splitting -R "." \
 --sample-sheet "SampleSheet.csv" -o ./Fastq
 
-# CONCATENER
-for i in *.fastq.gz;
-do cat /media/jbogoin/Data2/Donnees_brutes/hg19/Nephro/NEPHRO_HE_12012022_1/Fastq/$i \
-/media/jbogoin/Data2/Donnees_brutes/hg19/Nephro/NEPHRO_HE_12012022_2/Fastq/$i \
-> /media/jbogoin/Data2/Donnees_brutes/hg19/Nephro/NEPHRO_HE_12012022_CAT/$i;
-done
+# # CONCATENER
+# for i in *.fastq.gz;
+# do cat /media/jbogoin/Data2/Donnees_brutes/hg19/Nephro/NEPHRO_HE_12012022_1/Fastq/$i \
+# /media/jbogoin/Data2/Donnees_brutes/hg19/Nephro/NEPHRO_HE_12012022_2/Fastq/$i \
+# > /media/jbogoin/Data2/Donnees_brutes/hg19/Nephro/NEPHRO_HE_12012022_CAT/$i;
+# done
 
 echo ""
 echo "bcl2fastq.sh job done!"
