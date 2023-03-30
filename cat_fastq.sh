1f9232853716046fd586256e2c1ec8574a50d9a3
juugeebee <july2023@yahoo.fr>
2023-03-30 16:36:02 +0200

    rm

--- cat_fastq.sh
+++ cat_fastq.sh
@@ -4,9 +4,6 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate fastq_bam_env
 
 
-REF="/media/jbogoin/Data1/References/fa_hg19/hg19_std_M-rCRS_Y-PAR-mask.fa"
-
-
 echo ""
 echo "cat_fastq.sh start"
 echo ""
@@ -15,16 +12,22 @@ echo ""
 for i in *_R1_001.fastq.gz;
 
     do SAMPLE=${i%%_*};
-    cat "$SAMPLE"_S*_R1_001.fastq.gz > "$SAMPLE"_R1_001.fastq.gz;
+    echo $SAMPLE
+    cat "$SAMPLE"_S**_L00*_R1_001.fastq.gz > "$SAMPLE"_R1_001.fastq.gz;
 
 done
+
 
 for i in *_R2_001.fastq.gz;
 
     do SAMPLE=${i%%_*};
-    cat "$SAMPLE"_S*_R2_001.fastq.gz > "$SAMPLE"_R2_001.fastq.gz;
+    echo $SAMPLE
+    cat "$SAMPLE"_S**_L00*_R2_001.fastq.gz > "$SAMPLE"_R2_001.fastq.gz;
 
 done
+
+
+rm *_L**_R*.fastq.gz
 
 
 echo ""
