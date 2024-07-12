af8959ce4c078c941beb91bcfbbdb3d53796dc05
juugeebee <july2023@yahoo.fr>
2024-07-12 12:11:38 +0200

    cat fastq

--- cat_fastq.sh
+++ cat_fastq.sh
@@ -9,25 +9,25 @@ echo "cat_fastq.sh start"
 echo ""
 
 
-for i in *_R1_001.fastq.gz;
+for i in *_R1_001_A.fastq.gz;
 
     do SAMPLE=${i%%_*};
     echo $SAMPLE
-    cat "$SAMPLE"_S**_L00*_R1_001.fastq.gz > "$SAMPLE"_R1_001.fastq.gz;
+    cat "$SAMPLE"_R1_001_*.fastq.gz > "$SAMPLE"_R1.fastq.gz;
 
 done
 
 
-for i in *_R2_001.fastq.gz;
+for i in *_R2_001_A.fastq.gz;
 
     do SAMPLE=${i%%_*};
     echo $SAMPLE
-    cat "$SAMPLE"_S**_L00*_R2_001.fastq.gz > "$SAMPLE"_R2_001.fastq.gz;
+    cat "$SAMPLE"_R2_001_*.fastq.gz > "$SAMPLE"_R2.fastq.gz;
 
 done
 
 
-rm *_L**_R*.fastq.gz
+# rm *_L**_R*.fastq.gz
 
 
 echo ""
